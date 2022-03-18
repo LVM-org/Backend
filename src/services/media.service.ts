a2265fea884fa2da084af938afa4459268b3bb09
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2022-03-18 17:59:09 +0100

    fix: create new media error

--- src/services/media.service.ts
+++ src/services/media.service.ts
@@ -20,6 +20,7 @@ import { PROGRAM_ID } from '@/config';
 import { AccountLayout, mintTo, TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import BN from 'bn.js';
 import { logger } from '@/utils/logger';
+import ab2str from 'arraybuffer-to-string';
 
 @EntityRepository()
 class MediaService extends Repository<TokenEntity> {
@@ -267,96 +268,19 @@ class MediaService extends Repository<TokenEntity> {
     const filePath = '../Backend/media/' + mediaData.public_key + '.pdf';
     fs.readFile(filePath, async (err, data) => {
       if (!err) {
-        const fileContent = data;
+        const fileContent = data.toString('base64');
 
         const PdfDocument = await PDFDocument.load(fileContent);
 
-        const decoder = `importScripts("https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js")
-        importScripts("https://cdnjs.cloudflare.com/ajax/libs/pdf-lib/1.17.1/pdf-lib.min.js")
+        const decoder = `"${secret_key}"`;
 
-        const JsonFormatter = {
-          stringify: function (cipherParams) {
-            // create json object with ciphertext
-            const jsonObj = {
-            ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64),
-            };
-            // optionally add iv or salt
-            if (cipherParams.iv) {
-            jsonObj.iv = cipherParams.iv.toString();
-            }
-            if (cipherParams.salt) {
-            jsonObj.s = cipherParams.salt.toString();
-            }
-            // stringify json object
-            return JSON.stringify(jsonObj);
-          },
-          parse: function (jsonStr){
-            // parse json string
-            const jsonObj = JSON.parse(jsonStr);
-            // extract ciphertext from json object, and create cipher params object
-            const cipherParams = CryptoJS.lib.CipherParams.create({
-            ciphertext: CryptoJS.enc.Base64.parse(jsonObj.ct),
-            });
-            // optionally extract iv or salt
-            if (jsonObj.iv) {
-            cipherParams.iv = CryptoJS.enc.Hex.parse(jsonObj.iv);
-            }
-            if (jsonObj.s) {
-            cipherParams.salt = CryptoJS.enc.Hex.parse(jsonObj.s);
-            }
-            return cipherParams;
-          },
-        };
-
-
-        const getPage = async (pdfString, metadata, page) => {
-          const currentPdf = await PDFLib.PDFDocument.load(pdfString);
-          const newPdf = await PDFLib.PDFDocument.create();
-          newPdf.setTitle(metadata.title || '');
-          newPdf.setAuthor(metadata.author || '');
-          newPdf.setSubject(metadata.subject || '');
-          newPdf.setKeywords(metadata.keywords.split(","));
-          newPdf.setProducer(metadata.producer);
-          newPdf.setCreator(metadata.creator);
-          newPdf.setCreationDate(new Date(metadata.creationDate));
-          newPdf.setModificationDate(
-            new Date(metadata.modificationDate)
-          );
-          const copiedPages = await newPdf.copyPages(currentPdf, [page]);
-          newPdf.addPage(copiedPages[0]);
-          const pdfBytes = await newPdf.save();
-          return pdfBytes;
-        }
-
-
-        const decryptData = (encryptedData) => {
-          const encrypted = JsonFormatter.parse(encryptedData);
-          const decrypted = CryptoJS.AES.decrypt(encrypted, "${secret_key}", {
-            format: JsonFormatter,
-          });
-          return decrypted.toString(CryptoJS.enc.Utf8);
-        };
-
-
-
-        onmessage = function(data) {
-
-          const stringBuffer = new Uint8Array(data.data.file)
-          const pageNumber = data.data.page;
-          const metaDataToBuffer = new Uint8Array(data.data.metadata)
-          const metadata = JSON.parse(new TextDecoder().decode(metaDataToBuffer))
-          const stringifiedJson = new TextDecoder().decode(stringBuffer)
-          getPage(decryptData(stringifiedJson), metadata, pageNumber ).then((data) => {
-            postMessage(data);
-          })
-
-        };`;
+        logger.debug(secret_key);
 
         const decoderEncrptionKey = program_key + userPublicKey.toBase58();
 
         const encryptedDecoder = this.encryptFile(decoder, decoderEncrptionKey);
 
-        const encryptedPage = this.encryptFile(data.toString(), secret_key);
+        const encryptedPage = this.encryptFile(fileContent, secret_key);
 
         const spdfContent = `{
           "metadata": {
@@ -372,7 +296,7 @@ class MediaService extends Repository<TokenEntity> {
             "title": "${PdfDocument.getTitle()}"
             },
             "pages": ${encryptedPage},
-            "pubKey": ${program_key},
+            "pubKey": "${program_key}",
             "decoder": ${encryptedDecoder}
           }`;
 
